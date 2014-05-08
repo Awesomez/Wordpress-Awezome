3e5e7d855900a280cff90467bcb40136666381ca
Awesomez <likezyp@gmail.com>
2014-05-08 09:28:15 +0800

    added tag cloud to footer

--- footer.php
+++ footer.php
@@ -1,53 +1,19 @@
 <div class="footer section large-padding bg-dark">
-    <?php if (is_active_sidebar('footer-a')) : ?>
-        <div class="column column-1 left">
+    <?php if (is_active_sidebar('footer-a')) { ?>
+        <div class="column column-1">
             <div class="widgets">
                 <?php dynamic_sidebar('footer-a'); ?>
             </div>
         </div>
-    <?php else : ?>
-        <div class="column column-1 left">
-            <div class="widgets">
-                <div id="search" class="widget widget_search">
-                    <div class="widget-content">
-                        <h3 class="widget-title"><?php _e('Search form', 'wilson'); ?></h3>
-                        <?php get_search_form(); ?>
-                    </div>
-                </div>
-            </div>
-        </div>
-    <?php endif; ?> <!-- /footer-a -->
-
-    <?php if (is_active_sidebar('footer-b')) : ?>
-        <div class="column column-2 left">
+    <?php }?>
+    <?php if (is_active_sidebar('footer-b')) { ?>
+        <div class="column column-2">
             <div class="widgets">
                 <?php dynamic_sidebar('footer-b'); ?>
             </div>
             <!-- /widgets -->
         </div>
-
-    <?php else : ?>
-        <div class="column column-2 left">
-            <div class="widgets">
-                <div class="widget widget_recent_entries">
-                    <div class="widget-content">
-                        <h3 class="widget-title"><?php _e('Latest posts', 'wilson'); ?></h3>
-                        <ul>
-                            <?php
-                            $args = array('numberposts' => '5');
-                            $recent_posts = wp_get_recent_posts($args);
-                            foreach ($recent_posts as $recent) {
-                                echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="' . esc_attr($recent["post_title"]) . '" >' . $recent["post_title"] . '</a> </li> ';
-                            }
-                            ?>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <!-- /widgets -->
-
-        </div>
-    <?php endif; ?> <!-- /footer-b -->
+    <?php } ?>
     <div class="clear"></div>
 </div> <!-- /footer -->
 
